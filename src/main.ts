faeff09012bbc0ffa1c5e38864b72da9d747bab4
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-10-20 21:49:33 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import "./style.css";
 
 const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const header = document.createElement("h1");
+const canvas = document.createElement("canvas");
+
+canvas.height = canvas.width = 256;
+
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+header.innerHTML = APP_NAME;
+app.append(header);
+app.append(canvas);
+canvas.height = canvas.width = 256;
